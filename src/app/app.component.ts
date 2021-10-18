44aadb3c080b37e45ca568b3bc806f9b30fc21c0
adrien <adrien.mervelay84@gmail.com>
2021-10-18 10:16:42 +0200

    realisation de la quete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myFirsApp';
+  title = 'Welcome in the Adrien WebSite';
 }
